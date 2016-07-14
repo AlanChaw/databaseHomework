fe7a01a907ada1a152c7a43da3626706c31af5fc
alan <510729392@qq.com>
2016-07-14 22:13:57 +0800

    commit

--- src/main/java/com/expertWeb/model/ExpertExtened.java
+++ src/main/java/com/expertWeb/model/ExpertExtened.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Created by alan on 16/7/14.
  */
 public class ExpertExtened {
-    private Expert;
+    private Expert expert;
     private List<Qualification> qualificationList;
     private List<Review_exp> review_expList;
     private List<WorkExp> workExpList;
